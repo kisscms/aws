25637b0af03293a96f25985beef81c20038918c1
Makis Tracend <makis.tracend@gmail.com>
2012-07-31 02:04:52 -0700

    Adding s3_region config option

--- bin/config.php
+++ bin/config.php
@@ -9,6 +9,7 @@ if( class_exists('Config') && method_exists(new Config(),'register')){
 
 	// Register variables
 	Config::register("aws", "simpleDB_host", "sdb.us-west-1.amazonaws.com");
+	Config::register("aws", "s3_region", "s3-us-west-1.amazonaws.com");
 	
 }
 
