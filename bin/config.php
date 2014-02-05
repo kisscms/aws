57e644e088bc569f184d3528622adf84c9011ec3
Makis Tracend <makis.tracend@gmail.com>
2014-02-04 16:34:15 -0800

    enabling soft delete by default

--- bin/config.php
+++ bin/config.php
@@ -10,7 +10,7 @@ if( class_exists('Config') && method_exists(new Config(),'register')){
 	// Register variables
 	Config::register("aws", "simpleDB_host", "sdb.us-west-1.amazonaws.com");
 	Config::register("aws", "simpleDB_timestamps", "1");
-	Config::register("aws", "simpleDB_soft_delete", "0");
+	Config::register("aws", "simpleDB_soft_delete", "1");
 	Config::register("aws", "s3_region", "s3-us-west-1.amazonaws.com");
 
 }
