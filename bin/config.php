ed4318309a21f5dbb2b47c66623e00513f01bfab
Makis Tracend <makis.tracend@gmail.com>
2014-02-04 16:48:14 -0800

    updating default domains for sdb/s3

--- bin/config.php
+++ bin/config.php
@@ -8,10 +8,10 @@
 if( class_exists('Config') && method_exists(new Config(),'register')){
 
 	// Register variables
-	Config::register("aws", "simpleDB_host", "sdb.us-west-1.amazonaws.com");
+	Config::register("aws", "simpleDB_host", "sdb.amazonaws.com");
 	Config::register("aws", "simpleDB_timestamps", "1");
 	Config::register("aws", "simpleDB_soft_delete", "1");
-	Config::register("aws", "s3_region", "s3-us-west-1.amazonaws.com");
+	Config::register("aws", "s3_region", "s3.amazonaws.com");
 
 }
 
