c8ca9080831aa9dda6d2e69e8dc529808201e9a7
Makis Tracend <makis.tracend@gmail.com>
2014-02-04 17:45:00 -0800

    new config options to set credentials

--- bin/config.php
+++ bin/config.php
@@ -8,6 +8,9 @@
 if( class_exists('Config') && method_exists(new Config(),'register')){
 
 	// Register variables
+	Config::register("aws", "key", 			"01234567890");
+	Config::register("aws", "secret", 		"012345678901234567890123456789");
+	Config::register("aws", "region", 		"us-east-1");
 	Config::register("aws", "simpleDB_host", "sdb.amazonaws.com");
 	Config::register("aws", "simpleDB_timestamps", "1");
 	Config::register("aws", "simpleDB_soft_delete", "1");
