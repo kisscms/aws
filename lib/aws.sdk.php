fe15e7dd3273857f0b40aece44860bd76e3ee9e3
Makis Tracend <makis.tracend@gmail.com>
2025-05-12 17:17:24 +1000

    :warning: supressing AWS warnings

--- lib/aws.sdk.php
+++ lib/aws.sdk.php
@@ -1,5 +1,8 @@
 <?php
 
+// Suppress deprecation warnings: https://github.com/aws/aws-sdk-php/issues/2952
+define("AWS_SUPPRESS_PHP_DEPRECATION_WARNING", true);
+
 // Location of the AWS SDK
 require_once( SDK ."aws/3.165.0/aws.phar");
 
